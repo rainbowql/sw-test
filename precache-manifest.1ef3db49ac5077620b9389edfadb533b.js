cb9dee7b169180ff46293527450cbf6d1881c665
Lulu <76.qinlu@gmail.com>
2021-06-18 10:00:37 +0800

    初始化

---
+++ precache-manifest.1ef3db49ac5077620b9389edfadb533b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03811acd519db95ff291",
+    "url": "/css/app.d7690b55.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "9bd9c366db06312441e57a15ea3a4e8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdccab027fafc252018a",
+    "url": "/js/about.ff32cdf2.js"
+  },
+  {
+    "revision": "03811acd519db95ff291",
+    "url": "/js/app.41fadc09.js"
+  },
+  {
+    "revision": "6e8f663aa71ab372b40d",
+    "url": "/js/chunk-vendors.cdd0007a.js"
+  },
+  {
+    "revision": "29ac9856f70c82dd0a7baab3ae16398c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
